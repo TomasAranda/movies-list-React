3f167004532ed5317fc1bfc34ebb9398c20f772c
Tomas Aranda <tomas.aranda.95@gmail.com>
2020-11-03 12:18:54 -0300

    wip

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'build');
+const buildPath = path.join(__dirname, '..', 'build');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
@@ -14,8 +14,15 @@ app.use(
   })
 );
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+// app.get("/*", (req, res) => {
+//   let url = path.join(buildPath, 'index.html');
+//   if (!url.startsWith('/app/')) // since we're on local windows
+//     url = url.substring(1);
+//   res.sendFile(url);
+// });
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
 
 app.listen(port, () => {
